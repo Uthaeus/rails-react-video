96b0bf96b6fe2c901fe297c4323465c479af0cfc
Uthaeus <romanlavery@gmail.com>
2023-04-16 19:43:06 -0600

    created actions in projects controller, seeded database with dummy project data

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,13 +1,36 @@
 class Api::V1::ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :destroy]
+
   def index
+    project = Project.all.order(created_at: :desc)
+    render json: project
   end
 
   def create
+    project = Project.create!(project_params)
+    if project
+      render json: project
+    else
+      render json: project.errors
+    end
   end
 
   def show
+    render json: @project
   end
 
   def destroy
+    @project&.destroy
+    render json: { message: 'Project deleted!' }
+  end
+
+  private
+
+  def project_params
+    params.permit(:title, :description, :video_url, :thumbnail_url)
+  end
+
+  def set_project
+    @project = Project.find(params[:id])
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ApplicationRecord
-    validates :name, presence: true
+    validates :title, presence: true
     validates :description, presence: true
 end
