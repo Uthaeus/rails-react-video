0eaaed08cc728e8ee68aacab5a904b3431170f82
Uthaeus <romanlavery@gmail.com>
2023-04-17 09:23:38 -0600

    created contact and about pages, adding nav to layout

--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../components/Home";
 import Projects from "../components/Projects";
 import ProjectDetail from "../components/projectDetail";
+import ContactPage from "../components/pages/contact";
+import AboutPage from "../components/pages/about";
 
 export default (
     <Router>
@@ -11,6 +13,8 @@ export default (
             <Route path='/' exact element={<Home />} />
             <Route path='/projects' element={<Projects />} />
             <Route path='/project/:id' element={<ProjectDetail />} />
+            <Route path='/contact' element={<ContactPage />} />
+            <Route path='/about' element={<AboutPage />} />
         </Routes>
     </Router>
 );
