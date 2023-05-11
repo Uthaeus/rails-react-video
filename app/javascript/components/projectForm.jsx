3a64b332811944e3a2d81fabd1ccda7b18522b98
Uthaeus <romanlavery@gmail.com>
2023-05-10 18:56:33 -0600

    installed react hook form, converted project form

--- app/javascript/components/projectForm.jsx
+++ app/javascript/components/projectForm.jsx
@@ -1,66 +1,59 @@
 import React, { useState, useEffect } from "react";
-
+import { useForm } from "react-hook-form";
 
 
 function ProjectForm({ mode, data }) {
-    const [formState, setFormState] = useState({
-        title: "my title",
-        description: "",
-        thumbnail: "",
-        video_url: ""
-    });
+    const [isLoading, setIsLoading] = useState(true);
+    const { register, handleSubmit, reset, errors } = useForm();
 
     useEffect(() => {
         if (mode === "edit") {
-            
-            setFormState({
+            reset({
                 title: data.title,
                 description: data.description,
                 thumbnail: data.thumbnail,
-                video_url: data.video_url
+                video_url: data.video_url,
             });
         }
+        setIsLoading(false);
         console.log('useEffect')
     }, []);
-
-    const inputChangeHandler = (event) => {
-        const { name, value } = event.target;
-        setFormState({ ...formState, [name]: value });
-    };
 
     const submitHandler = (event) => {
         event.preventDefault();
     }
 
   return (
-    <form onSubmit={submitHandler}>
-        <p>here: {mode}</p>
-        <p>title: {formState.title}</p>
-        <p>description: {data.description}</p>
-        <p>thumbnail: {data.thumbnail}</p>
-        <p>video_url: {data.video_url}</p>
-        <div className="form-group">
-            <label htmlFor="projectTitle">Project Title</label>
-            <input type="text" id='projectTitle' className='form-control' value={formState.title} onChange={inputChangeHandler} />
-        </div>
+    isLoading ? <p>Loading...</p> 
+        : (
+        <form onSubmit={handleSubmit(submitHandler)}>
+            <div className="form-group">
+                <label htmlFor="title">Title</label>
+                <input type='text' {...register('title', { required: true })} className="form-control" />
+                {errors?.title && <span>This field is required</span>}
+            </div>
 
-        <div className="form-group">
-            <label htmlFor="projectDescription">Project Description</label>
-            <textarea id='projectDescription' className='form-control' value={formState.description} onChange={inputChangeHandler} rows={6} />
-        </div>
+            <div className="form-group">
+                <label htmlFor="description">Description</label>
+                <textarea {...register('description', { required: true })} className="form-control" rows={6} />
+                {errors?.description && <span>This field is required</span>}
+            </div>
 
-        <div className="form-group">
-            <label htmlFor="projectThumbnail">Project Thumbnail</label>
-            <input type="text" id='projectThumbnail' className='form-control' value={formState.thumbnail} onChange={inputChangeHandler} />
-        </div>
+            <div className="form-group">
+                <label htmlFor="thumbnail">Thumbnail</label>
+                <input type='text' {...register('thumbnail', { required: true })} className="form-control" />
+                {errors?.thumbnail && <span>This field is required</span>}
+            </div>
 
-        <div className="form-group">
-            <label htmlFor="projectVideoUrl">Project Video URL</label>
-            <input type="text" id='projectVideoUrl' className='form-control' value={formState.video_url} onChange={inputChangeHandler} />
-        </div>
+            <div className="form-group">
+                <label htmlFor="video_url">Video URL</label>
+                <input type='text' {...register('video_url', { required: true })} className="form-control" />
+                {errors?.video_url && <span>This field is required</span>}
+            </div>
 
-        <button type="submit" className="btn btn-primary">Submit</button>
-    </form>
+            <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+    )
   );
 }
 
